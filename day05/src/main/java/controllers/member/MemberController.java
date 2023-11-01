a972cdf9e95384ce92969993924f6cf8ea353533
gmkil11 <mkkil4451@gmail.com>
2023-11-01 15:38:57 +0900

    Spring - Spring thymeleaf

--- day05/src/main/java/controllers/member/MemberController.java
+++ day05/src/main/java/controllers/member/MemberController.java
@@ -9,6 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.IntStream;
+
 @Controller
 public class MemberController {
 /*    @Autowired
@@ -55,4 +59,26 @@ public class MemberController {
         return "member/info";
 
     }
+
+    @GetMapping("/member/list")
+    public String memberList(Model model){
+        List<Member> members = IntStream.rangeClosed(1, 10).mapToObj(this::addMember).toList();
+        model.addAttribute("members",members);
+        return "member/list";
+    }
+
+    private Member addMember(int i){
+
+        return Member.builder()
+                .userNo(i * 10000)
+                .userId("user" + i)
+                .userPw("123456")
+                .userNm("사용자" + i)
+                .email("user" + i + "@test.org")
+                .regDt(LocalDateTime.now())
+                .build();
+    }
+
+
+
 }
